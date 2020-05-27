f1d0f6a932ce66e5101a8a947b6171c2d9cf6826
karimb0 <karimbennis30@gmail.com>
2020-05-27 22:56:49 +0300

    added products-slider section

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,15 @@
 $(function () {
-
-
   // var mixer = mixitup(".products__inner-box");
+  $(".bottom-stars").rateYo({
+    rating: 4.5,
+    starWidth: "15px",
+    readOnly: true,
+  });
 
-  
+  $(".products-slider__inner").slick({
+    nextArrow:
+      '<button class="slick-arrow slick-next lnr lnr-chevron-right"></button>',
+    prevArrow:
+      '<button class="slick-arrow slick-prev lnr lnr-chevron-left"></button>',
+  });
 });
